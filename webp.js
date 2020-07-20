a862579acbf02aee1e4a098678819f6d12bccbe4
weru <onewesh@gmail.com>
2020-07-20 13:47:57 -0400

    update

--- webp.js
+++ webp.js
@@ -2,10 +2,9 @@ const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
 
 (async () => {
-  const files = await imagemin(['./static/images/*.{jpg,png}'], {
+  const files = await imagemin(['./static/images/*.{jpg,png,jpeg}'], {
     destination: './static/webp',
     plugins: [imageminWebp( {buffer: Buffer} )],
   });
-
-  console.log(files);
+  // console.log(files);
 })();
