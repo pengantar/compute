38d6a5e6ed4db8ea6cb09499b90bd7eaed062af4
weru <onewesh@gmail.com>
2020-09-10 10:56:12 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -558,7 +558,6 @@ $(function(){
   'use strict';
   
   // class helper functions from bonzo https://github.com/ded/bonzo
-  
   function classReg( className ) {
     return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
   }
